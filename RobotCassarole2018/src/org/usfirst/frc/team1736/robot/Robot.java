a986adb030737e8591fcf9130f2182a3ad11c04d
Chris Gerth <chrisgerth010592@gmail.com>
2018-01-05 14:52:15 -0600

    Integrated web server & calibration wrangler, cleaned up some code in robot.java

--- RobotCassarole2018/src/org/usfirst/frc/team1736/robot/Robot.java
+++ RobotCassarole2018/src/org/usfirst/frc/team1736/robot/Robot.java
@@ -7,9 +7,30 @@
 
 package org.usfirst.frc.team1736.robot;
 
+import org.usfirst.frc.team1736.lib.Calibration.CalWrangler;
+import org.usfirst.frc.team1736.lib.WebServer.CasseroleWebServer;
+
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/*
+ *******************************************************************************************
+ * Copyright (C) 2018 FRC Team 1736 Robot Casserole - www.robotcasserole.org
+ *******************************************************************************************
+ *
+ * This software is released under the MIT Licence - see the license.txt
+ *  file in the root of this repo.
+ *
+ * Non-legally-binding statement from Team 1736:
+ *  Thank you for taking the time to read through our software! We hope you
+ *   find it educational and informative! 
+ *  Please feel free to snag our software for your own use in whatever project
+ *   you have going on right now! We'd love to be able to help out! Shoot us 
+ *   any questions you may have, all our contact info should be on our website
+ *   (listed above).
+ *  If you happen to end up using our software to make money, that is wonderful!
+ *   Robot Casserole is always looking for more sponsors, so we'd be very appreciative
+ *   if you would consider donating to our club to help further STEM education.
+ */
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -20,6 +41,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends TimedRobot {
 	
+	// Software utilities
+	CasseroleWebServer webServer;
+	
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
@@ -27,18 +51,34 @@ public class Robot extends TimedRobot {
 	@Override
 	public void robotInit() {
 		
+		// Set up and start web server (must be after all other website init functions)
+		webServer = new CasseroleWebServer();
+		webServer.startServer();
+
+		// Load any saved calibration values (must be last to ensure all calibrations have been initialized first)
+		CalWrangler.loadCalValues();
+		
+	}
+	
+	/**
+	 * This function is called just before the robot enters disabled
+	 */
+	@Override
+	public void disabledInit() {
+		
+		
+	}
+	
+	/**
+	 * This function is called periodically while disabled. 
+	 */
+	@Override
+	public void disabledPeriodic() {
+		
 	}
 
 	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * <p>You can add additional auto modes by adding additional comparisons to
-	 * the switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
+	 * This function is called just before the robot enters autonomous
 	 */
 	@Override
 	public void autonomousInit() {
