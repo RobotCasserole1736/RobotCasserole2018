a869ab2fd4fe8a9202590142265687c25ffa1d53
GLADOS <glados@robotcasserole.org>
2018-01-15 15:44:46 -0600

    not nearly done yet

--- RobotCassarole2018/src/org/usfirst/frc/team1736/robot/Drivetrain.java
+++ RobotCassarole2018/src/org/usfirst/frc/team1736/robot/Drivetrain.java
@@ -1,24 +1,20 @@
 package org.usfirst.frc.team1736.robot;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
 public class Drivetrain {
 	private static Drivetrain singularInstance = null;
-	private TalonSRX motorleft1;
-	private TalonSRX motorleft2;
-	private TalonSRX motorleft3;
-	private TalonSRX motorright1;
-	private TalonSRX motorright2;
-	private TalonSRX motorright3;
+	
 	
 	public static synchronized Drivetrain getInstance() {
 		if ( singularInstance == null)
 			singularInstance = new Drivetrain();
 		return singularInstance;
 	}
+
 	
 	
 	private Drivetrain() {
+		
 
 	}
 }
+
---
+++ RobotCassarole2018/src/org/usfirst/frc/team1736/robot/Gearbox.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team1736.robot;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class Gearbox {
+	private TalonSRX motor1;
+	private TalonSRX motor2;
+	private TalonSRX motor3;
+	{
+}
+					 }
+
+
+
