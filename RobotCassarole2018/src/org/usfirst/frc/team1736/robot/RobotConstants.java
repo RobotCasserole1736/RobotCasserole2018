0d871ef284ac414c404426d6680d919d531e6571
SPARKY <sparky@robotcasserole.org>
2018-01-20 10:36:52 -0600

    creating the RobotConstants list

---
+++ RobotCassarole2018/src/org/usfirst/frc/team1736/robot/RobotConstants.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team1736.robot;
+
+public class RobotConstants {
+	public static final int INTAKE_PROXIMITY_SENSOR = 4;
+	public static final int INTAKE_SPARK_SENSOR_ONE = 0;
+	public static final int INTAKE_SPARK_SENSOR_TWO = 1;
+	public static final int INTAKE_SERVO_ONE = 8;
+	public static final int INTAKE_SERVO_TWO = 9;
+	public static final int ELEVATER_ENCODER_ONE = 5;
+	public static final int ELEVATER_ENCODER_TWO = 6;
+	public static final int ELEVATER_PROXIMITY_SENSOR = 7;
+	public static final int ELEVATER_VICTOR_SP_ONE = 2;
+	public static final int ELEVATER_VICTOR_SP_TWO = 3;
+	public static final int ELEVATER_ANALOG_ONE = 0;
+	public static final int ELEVATER_ANALOG_TWO = 1;
+	public static final int CLIMBER_SINGLE_CHANNEL_ENCODER_ONE = 8;
+	public static final int CLIMBER_SINGLE_CHANNEL_ENCODER_TWO = 9;
+	public static final int CLIMBER_SPARK_ONE = 4;
+	public static final int CLIMBER_SPARK_TWO = 5;
+	public static final int CLIMBER_SPARK_THREE = 6;
+	public static final int CLIMBER_SPARK_FOUR = 7;
+}
+
