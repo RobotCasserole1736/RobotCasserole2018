d313d7876815b8f2e432d28e1e0469fcb2a453e1
Chris Gerth <chrisgerth010592@gmail.com>
2018-01-22 22:29:14 -0600

    Updated robot constants per latest known info (but still lots of guesses)

--- RobotCassarole2018/src/org/usfirst/frc/team1736/robot/RobotConstants.java
+++ RobotCassarole2018/src/org/usfirst/frc/team1736/robot/RobotConstants.java
@@ -1,23 +1,40 @@
 package org.usfirst.frc.team1736.robot;
 
 public class RobotConstants {
-	public static final int INTAKE_PROXIMITY_SENSOR = 4;
-	public static final int INTAKE_SPARK_SENSOR_ONE = 0;
-	public static final int INTAKE_SPARK_SENSOR_TWO = 1;
-	public static final int INTAKE_SERVO_ONE = 8;
-	public static final int INTAKE_SERVO_TWO = 9;
-	public static final int ELEVATER_ENCODER_ONE = 5;
-	public static final int ELEVATER_ENCODER_TWO = 6;
-	public static final int ELEVATER_PROXIMITY_SENSOR = 7;
+	
+	//PWM Outputs
+	public static final int INTAKE_SPARK_LEFT = 0;
+	public static final int INTAKE_SPARK_RIGHT = 1;
 	public static final int ELEVATER_VICTOR_SP_ONE = 2;
 	public static final int ELEVATER_VICTOR_SP_TWO = 3;
-	public static final int ELEVATER_ANALOG_ONE = 0;
-	public static final int ELEVATER_ANALOG_TWO = 1;
-	public static final int CLIMBER_SINGLE_CHANNEL_ENCODER_ONE = 8;
-	public static final int CLIMBER_SINGLE_CHANNEL_ENCODER_TWO = 9;
 	public static final int CLIMBER_SPARK_ONE = 4;
 	public static final int CLIMBER_SPARK_TWO = 5;
 	public static final int CLIMBER_SPARK_THREE = 6;
 	public static final int CLIMBER_SPARK_FOUR = 7;
+	public static final int ELBOW_SPARK = 8;
+	
+	
+	//CAN ID's
+	public static final int DRIVETRAIN_LEFT_MASTER_SRX = 0;
+	public static final int DRIVETRAIN_LEFT_SLAVE1_SRX = 1;
+	public static final int DRIVETRAIN_LEFT_SLAVE2_SRX = 2;
+	public static final int DRIVETRAIN_RIGHT_MASTER_SRX = 3;
+	public static final int DRIVETRAIN_RIGHT_SLAVE1_SRX = 4;
+	public static final int DRIVETRAIN_RIGHT_SLAVE2_SRX = 5;
+	
+	
+	//Digital Inputs
+	public static final int ELBOW_UP_LIMIT_SW = 0;
+	public static final int ELBOW_DOWN_LIMIT_SW = 1;
+	public static final int INTAKE_CUBE_PRESENT_SENSOR = 4;
+	public static final int ELEVATER_ENCODER_A = 5;
+	public static final int ELEVATER_ENCODER_B = 6;
+	public static final int ELEVATER_LOWER_LIMIT_SW = 7;
+	public static final int ELEVATER_UPPER_LIMIT_SW = 8;
+	
+	//Analog Inputs
+	
+	//PDP Hookups
+	
 }
 
