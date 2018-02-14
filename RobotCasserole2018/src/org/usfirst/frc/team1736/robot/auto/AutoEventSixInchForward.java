82ec4c918c00e1ad4040b6eeec91c590056e42e2
ADA <ada@robotcasserole.org>
2018-02-13 19:24:12 -0600

    Go six inches forward

---
+++ RobotCasserole2018/src/org/usfirst/frc/team1736/robot/auto/AutoEventSixInchForward.java
@@ -0,0 +1,54 @@
+package org.usfirst.frc.team1736.robot.auto;
+
+import org.usfirst.frc.team1736.lib.AutoSequencer.AutoEvent;
+import org.usfirst.frc.team1736.lib.PathPlanner.FalconPathPlanner;
+import org.usfirst.frc.team1736.lib.PathPlanner.PathPlannerAutoEvent;
+
+/**
+ * go to scale on left.
+ */
+public class AutoEventSixInchForward extends AutoEvent {
+	PathPlannerAutoEvent driveForward;
+
+	private final double[][] waypoints = new double[][] {
+		{0, 0},
+		{0, 6}, 
+		
+	};
+	
+	private final double time = 2.0;
+
+	public AutoEventSixInchForward() {
+		driveForward = new PathPlannerAutoEvent(waypoints, time);
+		driveForward.path.setVelocityAlpha(0.001);
+	}
+
+	@Override
+	public void userUpdate() {
+		driveForward.userUpdate();
+		// shotCTRL.setDesiredShooterState(ShooterStates.PREP_TO_SHOOT);
+	}
+	@Override
+	public void userForceStop() {
+		driveForward.userForceStop();
+	}
+
+	@Override
+	public boolean isTriggered() {
+		return driveForward.isTriggered();
+	}
+
+	@Override
+	public boolean isDone() {
+		return driveForward.isDone();
+	}
+
+	@Override
+	public void userStart() {
+		driveForward.userStart();
+	}
+    public static void main(String[] args) {
+		AutoEventSixInchForward autoEvent = new AutoEventSixInchForward();
+		FalconPathPlanner.plotPath(autoEvent.driveForward.path);
+	}
+}
