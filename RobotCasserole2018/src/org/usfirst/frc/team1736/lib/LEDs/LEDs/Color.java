0310f6e0ba27e1274a22c042b13cc03e9e6b01dc
ADA <ada@robotcasserole.org>
2018-04-19 20:20:23 -0500

    some LED files

---
+++ RobotCasserole2018/src/org/usfirst/frc/team1736/lib/LEDs/LEDs/Color.java
@@ -0,0 +1,82 @@
+package org.usfirst.frc.team1736.lib.LEDs.LEDs;
+
+import java.util.Set;
+
+public class Color {
+	private double H_Value;
+	private double S_Value;
+	private double L_Value;
+	
+	
+	public Color() {
+		H_Value = 0;
+		S_Value = 1;
+		L_Value = .5;
+	}
+	
+	
+	
+	public void setLevel(int Level) {
+		if (Level == 0)/*mooo*/{
+			H_Value = 0;
+			S_Value = 0;
+			L_Value = 0;
+		} else if (Level == 1) {
+		 	H_Value = 0;
+		 	S_Value = 1;
+		 	L_Value = .5;
+		} else if (Level == 2) {
+			H_Value = -0;
+			S_Value = 1;
+			L_Value = .5;
+		} else if (Level == 3) {
+			H_Value = 0;
+			S_Value = 1;
+			L_Value = .5;
+		} else if (Level == 4) {
+			H_Value = 0;
+			S_Value = 1;
+			L_Value = .5;
+		}
+	}
+	
+	public double getH_Value() {
+		return H_Value;
+	}
+	
+	public double getS_Value() {
+		return S_Value;
+	}
+	
+	public double getL_Value() {
+		return L_Value;
+	}
+	
+	public void setL_Value(double in) {
+		L_Value = in;
+	}
+
+		
+	public void addToMe(Color in) {
+		double newH = in.getH_Value();
+		double heldH = H_Value;
+		double avrgpt1 = heldH + newH;
+		H_Value = (avrgpt1/2);
+		S_Value = 1;
+		L_Value += in.L_Value;
+		 if (L_Value > .52) {
+			 L_Value = .52;
+			 H_Value = H_Value + .072;
+		 }
+		
+	}
+	
+		
+
+	
+	
+		
+		
+	}
+	
+
--- RobotCasserole2018/src/org/usfirst/frc/team1736/lib/LEDs/Particle.java
+++ RobotCasserole2018/src/org/usfirst/frc/team1736/lib/LEDs/Particle.java
@@ -2,52 +2,78 @@ package org.usfirst.frc.team1736.lib.LEDs;
 
 public class Particle {
 //Number of the LED the center of the particle is occupying
-	private int position;
+	private double position;
 //# of LEDs moved per cycle
-	private int speed;
+	private double speed;
 //# of LEDs taken up by the particle
 	private int width;
 //Sort of a multiplier,1 but also not
 	private int level;
 //# of times called
 	private int callNum;
- 
+//Color 
+	private Color On_Color; 
+	private Color Off_Color;
 //Age is # of cycles the particle has existed for.
-
+	private double age;
 	
 	
-	public Particle() {
+	public Particle(int levelin) {
 		double time = (callNum * 50);
-		
-		int Color = level;
+		double ledNum = position;
+		On_Color = new Color();
+		On_Color.setLevel(levelin);
+		Off_Color = new Color();
+		Off_Color.setLevel(0);
 		speed = 1;
 		width = 4;
-			
-		
-		
-		
-	}
+	}	
+	
 	public void move() {
+		double Lval = 0;
+		double Nval = 0;
+		Lval = 0;
 		position = position + speed;
-		if (position > 24) {
+		System.out.println(position);
+		if (level == 1 && position > 8) {
+			reset();
+		}
+		
+		if (position > 18 | position < 24) {
+				Lval = On_Color.getL_Value();
+				Nval = Lval - Lval / 15;
+				On_Color.setL_Value(Nval);
+		}
+		
+		if (position > 24 | position < 0) {
 			reset();
 		}
 	}
+
 	
 	
-	public double colorAt(int idx) {
-		int lower = position;
-		int upper = position + width;
+
+	
+	public Color ColorAt(int idx) {
+		double lower = position;
+		double upper = position + width;
 		
 		if (idx < upper & idx > lower) {
-			return .25;
+			return On_Color;
 		} else {
-			return 0;
+			return Off_Color;
 		}
 	}
 	
 	public void reset() {
 		position = 0;
+		speed += Math.random() - .5;
+		On_Color.setL_Value(.5);
+		if (speed > 1.75) {
+			speed = 1.75;
+		} else if (speed < .25) {
+			speed = .25;
+		}
 	}
 	
 }
